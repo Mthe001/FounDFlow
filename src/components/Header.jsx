7133aab737c12e526ac406b4ada20c7444aa0a88
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-11-18 08:03:18 +0600

    fix:home and feat:image-in-readme.md

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Carousel from './Carousel';
 
 const Header = () => {
     return (
-        <div className='border border-red-500'>
+        <div className='p-5'>
             <section className=''>
                 <section className='title text-center'>
                     <h1 className=' lg:text-3xl md:text-2xl text-xl  font-semibold text-slate-600'>Winter is not easy for   <span className='text-red-500'>Some</span><span className='text-orange-500'>!</span></h1>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,7 @@ const Home = () => {
     return (
         <div>
 
-            <h1>Home</h1>
-            <h1>carosel in the header perent is home</h1>
+
             <Header />
 
             {/* how it wokrs */}
